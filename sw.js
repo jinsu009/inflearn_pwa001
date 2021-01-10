db6bbad2f0f8748ee447c28b0881122305e1ee4e
cmj <chmin82@gmail.com>
2021-01-10 20:12:32 +0900

    서비스 워커 설치 - install 이벤트와 self

---
+++ sw.js
@@ -0,0 +1,11 @@
+// var CACHE_NAME = 'cache-v1';
+// var fileToCache =[
+//     '/',
+//     '/js/app.js',
+//     '/css/base.css'
+// ];
+
+// 서비스 워커 설치 (웹 자원 캐싱)
+self.addEventListener('install', (event) => {
+  
+});
